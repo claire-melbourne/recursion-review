3e95d818e39af28c3f0d7a821a3953817a20da03
Claire Melbourne <claire.melbourne@gmail.com>
2020-08-17 14:05:23 -0500

    drafted get elements function

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,8 +4,22 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
-  // your code here
-  //begin
+var getElementsByClassName = function(className, node = document.body, siblings = []) {
+  var classes = [].slice().call(node.classList);
+  if (classes.includes(className)) {
+    var part = [node];
+  } else {
+    var part = [];
+  }
+  var children = [].slice().call(node.children);
+  if (children.length === 0 && siblings.length === 0) {
+    return part;
+  }
+  if (children.length > 0 && siblings.length === 0) {
+    return part.concat(getElementsByClassName(className, children[0], children.slice(1)));
+  }
+  if (children.length === 0 && siblings.length > 0) {
+    return part.concat(getElementsByClassName(className, siblings[0], siblings.slice(1)));
+  }
+  return part.concat(getElementsByClassName(className, children[0], children.slice(1))).concat(getElementsByClassName(className, siblings[0], siblings.slice(1)));
 };
