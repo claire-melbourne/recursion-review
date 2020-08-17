ea17081c88e503d17485fb5903c859e25c917572
Claire Melbourne <claire.melbourne@gmail.com>
2020-08-17 18:31:17 -0500

    began JSON parse

--- src/parseJSON.js
+++ src/parseJSON.js
@@ -4,4 +4,33 @@
 // but you're not, so you'll write it from scratch:
 var parseJSON = function(json) {
   // your code goes here
+  if (json[0] === '"') {
+    return json.slice(1, json.length - 1);
+  }
+  if (json === 'true') {
+    return true;
+  }
+  if (json === 'false') {
+    return false;
+  }
+  if (json === 'null') {
+    return null;
+  }
+  if (!isNaN(Number(json))) {
+    return Number(json);
+  }
+  if (json === '[]') {
+    return [];
+  }
+  if (json[0] === '[') {
+    var results = [];
+    json = json.slice(0, json.length - 1);
+    var parts = json.split(',');
+    for (var i = 0; i < parts.length; i++) {
+      // do some stuff
+    }
+  }
 };
+
+
+//
