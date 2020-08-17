54db1eeffe6fb3f9bc66c91b1eb6684b152210fc
Claire Melbourne <claire.melbourne@gmail.com>
2020-08-17 17:25:55 -0500

    stringify objects

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -6,19 +6,41 @@
 var stringifyJSON = function(obj) {
   // your code goes here
 
+  if (obj === null) {
+    return 'null';
+  }
+
   if (typeof obj === 'string') {
     return '"' + obj + '"';
   }
   if (Array.isArray(obj)) {
     var str = '[';
 
-    if(obj.length === 0) {
+    if (obj.length === 0) {
       return '[]';
     }
     for (var i = 0; i < obj.length; i++) {
       str += stringifyJSON(obj[i]) + ',';
     }
     str = str.slice(0, str.length - 1) + ']';
+    return str;
+  }
+
+  if (typeof obj === 'function' || obj === undefined) {
+    return '{}';
+  }
+
+  if (typeof obj === 'object') {
+
+    if (Object.keys(obj).length === 0) {
+      return '{}';
+    }
+
+    var str = '{';
+    for (var key in obj) {
+      str += stringifyJSON(key) + ':' + stringifyJSON(obj[key]) + ',';
+    }
+    str = str.slice(0, str.length - 1) + '}';
     return str;
   }
 
